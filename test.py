8e3ed088d522b97780a8a3a0eb0c952d68a2e2ff
disneyresidents <daikonmotonaga@gmail.com>
2020-03-26 12:53:52 +0900

    テスト用ファイルを削除

--- test.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome(r"C:\Users\daiko\Desktop\bot-cog\chromedriver.exe")
-driver.get("http://scp-jp.wikidot.com/scp-2000-jp")
-# driver.close()
-# driver.quit()
