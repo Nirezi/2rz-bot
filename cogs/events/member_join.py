ee5d1119d4cba8a070eadc6feb016f975c016ae2
disneyresidents <daikonmotonaga@gmail.com>
2020-03-26 12:52:23 +0900

    2rz鯖でのmember_join時の動作の変更

--- cogs/events/member_join.py
+++ cogs/events/member_join.py
@@ -20,12 +20,17 @@ class member_join(commands.Cog):
                 await channel.send(f"いらっしゃい!{member.mention}さん！ :tada:\n<#630402461395451913>を読んでください！")
 
         if server == 621326525521723414:  # 2rezi
-            channel = client.get_channel(625288084648361993)  # 参加時のメッセージ用
-            welcome = f"2レジ鯖へようこそ！{member.mention}さん :tada:\n"
-            welcome += "<#672010326077734922>でコマンドを入力してください！\n"
-            welcome += "整地鯖やってる人はmcidも<#621328380620701736>でお願いします"
-            # mcid申請のch
-            await channel.send(welcome)
+            if member.id == 447376081247404043:
+                channel = client.get_channel(625288084648361993)
+                await channel.send(f"{member.mention}入れると思ったんですか？kickしますね")
+                await member.guild.kick(member, reason="言わずもがな")
+            else:
+                channel = client.get_channel(625288084648361993)  # 参加時のメッセージ用
+                welcome = f"2レジ鯖へようこそ！{member.mention}さん :tada:\n"
+                welcome += "<#672010326077734922>でコマンドを入力してください！\n"
+                welcome += "整地鯖やってる人はmcidも<#621328380620701736>でお願いします"
+                # mcid申請のch
+                await channel.send(welcome)
 
         if server == 615394790669811732:  # タコ柴
             channel = client.get_channel(615394790669811734)  # join
