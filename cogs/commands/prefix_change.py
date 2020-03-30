cc757f7d1e8408d05cfc2f3d67e8e57fd382296b
disneyresidents <daikonmotonaga@gmail.com>
2020-03-31 08:03:19 +0900

    custom prefixへの対応

--- cogs/commands/prefix_change.py
+++ cogs/commands/prefix_change.py
@@ -1,8 +1,19 @@
-import sqlite3 as sql
+import os
 
+import psycopg2
 from discord.ext import commands
 
-db = sql.connect("prefixes.db")
+try:
+    import tokens
+    local = True
+except ModuleNotFoundError:
+    local = False
+
+if local:
+    SQLpath = tokens.PostgreSQL
+else:
+    SQLpath = os.environ["postgre"]
+db = psycopg2.connect(SQLpath)
 cur = db.cursor()
 
 
@@ -18,33 +29,33 @@ class prefix(commands.Cog):
 
     @prefix.command()
     async def change(self, ctx, new_prefix):
-        cur.execute("select * from prefixes WHERE guild_id = ?", (ctx.guild.id,))
+        cur.execute("select * from prefixes WHERE guild_id = %s", (ctx.guild.id,))
         kazu = len(cur.fetchall())
         if kazu == 0:
-            cur.execute("INSERT INTO prefixes values (?, ?)", (ctx.guild.id, new_prefix))
+            cur.execute("INSERT INTO prefixes values (%s, %s)", (ctx.guild.id, new_prefix))
             await ctx.send(f"prefixが`/`から`{new_prefix}`に変更されました")
         else:
-            cur.execute("DELETE FROM prefixes WHERE guild_id = ?", (ctx.guild.id,))
-            cur.execute("INSERT INTO prefixes values (?, ?)", (ctx.guild.id, new_prefix))
+            cur.execute("DELETE FROM prefixes WHERE guild_id = %s", (ctx.guild.id,))
+            cur.execute("INSERT INTO prefixes values (%s, %s)", (ctx.guild.id, new_prefix))
             await ctx.send(f"prefixが`{ctx.prefix}`から`{new_prefix}`に変更されました")
         db.commit()
 
     @prefix.command()
     async def default(self, ctx):
-        cur.execute("DELETE FROM prefixes WHERE guild_id = ?", (ctx.guild.id,))
-        cur.execute("INSERT INTO prefixes values (?, '/')", (ctx.guild.id,))
+        cur.execute("DELETE FROM prefixes WHERE guild_id = %s", (ctx.guild.id,))
+        cur.execute("INSERT INTO prefixes values (%s, '/')", (ctx.guild.id,))
         db.commit()
         await ctx.send("prefixがデフォルトの`/`に変更されました")
 
     @prefix.command()
     async def show(self, ctx):
-        cur.execute("select * from prefixes WHERE guild_id = ?", (ctx.guild.id,))
+        cur.execute("select * from prefixes WHERE guild_id = %s", (ctx.guild.id,))
         kazu = len(cur.fetchall())
         if kazu == 0:
             await ctx.send(f"{ctx.guild.name}でのprefixは`/`です")
         else:
             cur.execute(
-                "select * from prefixes WHERE guild_id = ?", (ctx.guild.id,))
+                "select * from prefixes WHERE guild_id = %s", (ctx.guild.id,))
             pre = ""
             for row in cur.fetchall():
                 pre += row[1]
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
 import os
 import traceback
-import sqlite3 as sql
 
 import discord
+import psycopg2
 from discord.ext import commands
 
 # loop = asyncio.new_event_loop()
@@ -16,9 +16,15 @@ except ModuleNotFoundError:
     token1 = os.environ["token1"]
     local = False
 
-db = sql.connect("prefixes.db")
+if not local:
+    SQLpath = os.environ["postgre"]
+else:
+    SQLpath = tokens.PostgreSQL
+
+db = psycopg2.connect(SQLpath)
 cur = db.cursor()
 cur.execute("select * from prefixes")
+cant_connect_db = False
 
 
 def _prefix_callable(bot, msg):
@@ -26,13 +32,18 @@ def _prefix_callable(bot, msg):
     if msg.guild is None:
         base.append('/')
     else:
-        cur.execute("select prefix from prefixes WHERE guild_id = ?", (msg.guild.id,))
-        if len(cur.fetchall()) == 0:
+        try:
+            cur.execute("select prefix from prefixes WHERE guild_id = %s", (msg.guild.id,))
+            if len(cur.fetchall()) == 0:
+                base.append("/")
+            else:
+                cur.execute("select * from prefixes where guild_id = %s", (msg.guild.id,))
+                for row in cur.fetchall():
+                    base.append(str(row[1]))
+        except Exception:
             base.append("/")
-        else:
-            cur.execute("select * from prefixes where guild_id = ?", (msg.guild.id,))
-            for row in cur.fetchall():
-                base.append(str(row[1]))
+            global cant_connect_db
+            cant_connect_db = True
     return base
 
 
@@ -67,6 +78,10 @@ class mybot(commands.Bot):
         print(bot.user.id)
         print(discord.__version__)
         print("--------")
+
+        if cant_connect_db:
+            ch = self.get_channel(639121643901288500)
+            await ch.send("<@544774774405201923>/nPostgreSQLへの接続に失敗しました。一時的にprefixを`/`に設定しています")
 
         while not self.is_closed():
             kazu = len(list(self.get_all_members()))
