77fca25ca0ec32f71a47cb43350e6ae150ed76fd
disneyresidents <disneyresidents@gmail.com>
2020-05-16 21:20:01 +0900

    [Rem]sub_keyの概念を廃止 (要素ごとファイル分けてあげたほうがわかりやすいからね)
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -21,31 +21,17 @@ class Config:
         except Exception:
             pass
 
-    async def put(self, key, value, sub_key=None):
-        if sub_key is None:
-            self._file[key] = value
-            await self._dump()
-        else:
-            if key not in self._file.keys():
-                self._file[key] = {}
-            self._file[key][sub_key] = value
-            await self._dump()
+    async def put(self, key, value):
+        self._file[key] = value
+        await self._dump()
 
-    async def remove(self, key, sub_key=None):
-        if sub_key is None:
-            value = self._file.pop(key)
-            await self._dump()
-            return value
-        else:
-            value = self._file[key].pop(sub_key)
-            await self._dump()
-            return value
+    async def remove(self, key):
+        value = self._file.pop(key)
+        await self._dump()
+        return value
 
     def get(self, key, *args):
         return self._file.get(str(key), *args)
 
-    def keys(self, key=None):
-        if key is None:
-            return self._file.keys()
-        return self._file[key].keys()
-
+    def keys(self):
+        return self._file.keys()
