be177cb12e97fd2fb42b01ed66519972a6a91fc1
disneyresidents <disneyresidents@gmail.com>
2020-05-21 22:56:54 +0900

    [Fix]typo修正と、check関数の定義位置の変更
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/commands/bug_report.py
+++ cogs/commands/bug_report.py
@@ -9,7 +9,7 @@ class Report(commands.Cog):
 
     @commands.command()
     async def bug_report(self, ctx):
-        anser_list = []
+        answer_list = []
         msg_list = []
         msg = await ctx.send("バグ等の報告ですね。各項目10分以内に送信してください")
         msg_list.append(msg)
@@ -18,12 +18,13 @@ class Report(commands.Cog):
                         "その現象は何をすると発生しますか？\n例)/avatar コマンドを使うと",
                         "具体的にどのような現象ですか？",
                         "それ以外に伝えておきたいことはありますか？(なければ「なし」で構いません)"]
+
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
         for koumoku in koumoku_list:
             send_msg = await ctx.send(koumoku)
             msg_list.append(send_msg)
-
-            def check(m):
-                return m.author == ctx.author and m.channel == ctx.channel
 
             try:
                 anser = await self.bot.wait_for("message", timeout=600, check=check)
@@ -31,16 +32,16 @@ class Report(commands.Cog):
                 await ctx.send("タイムアウトしました。はじめからやり直してください")
                 return
             else:
-                anser_list.append(anser.content)
+                answer_list.append(anser.content)
                 msg_list.append(anser)
 
         report_em = discord.Embed(
             description=f'**New Report in {ctx.channel.mention}**\n\n',
             color=0xff0000)  # 発言内容をdescriptionにセット
-        report_em.add_field(name="発生場所", value=anser_list[0])
-        report_em.add_field(name="何をすると", value=anser_list[1])
-        report_em.add_field(name="具体的な現象", value=anser_list[2])
-        report_em.add_field(name="備考", value=anser_list[3])
+        report_em.add_field(name="発生場所", value=answer_list[0])
+        report_em.add_field(name="何をすると", value=answer_list[1])
+        report_em.add_field(name="具体的な現象", value=answer_list[2])
+        report_em.add_field(name="備考", value=answer_list[3])
         await ctx.send("これでよろしいでしょうか？\nyesかnoで答えてください", embed=report_em)
 
         try:
@@ -51,7 +52,7 @@ class Report(commands.Cog):
         else:
             kakunin = kakunin_kekka.content.lower()
             if kakunin == "yes":
-                embed = discord.Emebed(
+                embed = discord.Embed(
                     title="報告ありがとうございます",
                     description="公式サーバの方では進捗を確認できます。\n[公式サーバ](https://discord.gg/bQWsu3Z)")
                 await ctx.send(embed=embed)
