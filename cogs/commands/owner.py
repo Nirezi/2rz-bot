5d146692e50633abbcab5a73ab7be3b9f472f311
disneyresidents <disneyresidents@gmail.com>
2020-05-27 20:52:42 +0900

    [Add]botのブラックリストに関する機能を追加(owner限定)
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

---
+++ cogs/commands/owner.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+
+
+class Owner(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_check(self, ctx):
+        return await self.bot.is_owner(ctx.author)
+
+    @commands.group()
+    async def black_list(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f"ブラックリストには以下のidが登録されてるよ！\n{self.bot.black_list.keys()}")
+
+    @black_list.command()
+    async def add(self, ctx, key: str):
+        await self.bot.black_list.put(key, True)
+        await ctx.send(f"{key}をブラックリストに追加しました")
+
+    @black_list.command()
+    async def remove(self, ctx, key: str):
+        if key in self.bot.black_list.keys():
+            await ctx.send(f'{key}はまだブラックリストに登録されていません')
+            return
+        value = await self.bot.black_list.remove(key)
+        await ctx.send(f"{value}をブラックリストから削除しました")
+
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
