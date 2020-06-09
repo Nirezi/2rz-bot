3b1dfd76c2c61efb08e4fb3e765c0890fcfac8d1
disneyresidents <disneyresidents@gmail.com>
2020-06-09 21:25:44 +0900

    [Add]サブキーの取り扱いのための機能を追加
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -7,31 +7,43 @@ class Config:
         self.name = name
         self.loop = asyncio.get_event_loop()
         self.lock = asyncio.Lock()
-        self._file = {}
-        self.load_file()
+
+        try:
+            with open(self.name, 'r') as f:
+                self._file = json.load(f)
+        except Exception:
+            self._file = {}
 
     async def _dump(self):
         with open(self.name, 'w', encoding='utf-8') as f:
             json.dump(self._file.copy(), f, ensure_ascii=True, separators=(',', ':'))
 
-    def load_file(self):
-        try:
-            with open(self.name, 'r') as f:
-                self._file = json.load(f)
-        except Exception:
-            pass
-
-    async def put(self, key: str, value: str):
-        self._file[str(key)] = value
+    async def put(self, key, value, sub_key=None):
+        if sub_key is None:
+            self._file[str(key)] = value
+        else:
+            if str(key) not in self._file.keys():
+                self._file[str(key)] = {}
+            self._file[str(key)][str(sub_key)] = value
         await self._dump()
 
-    async def remove(self, key: str):
-        value = self._file.pop(str(key))
+    async def remove(self, key, sub_key=None):
+        if sub_key is None:
+            value = self._file.pop(str(key))
+        else:
+            value = self._file[str(key)].pop(str(sub_key))
         await self._dump()
         return value
 
-    def get(self, key: str, *args: str):
-        return self._file.get(str(key), *args)
+    def get(self, key, args, sub_key=None):
+        if sub_key is None:
+            return self._file.get(str(key), args)
+        return self._file[str(key)].get(str(sub_key), args)
 
-    def keys(self):
-        return self._file.keys()
+    def keys(self, key=None):
+        if key is None:
+            return self._file.keys()
+        else:
+            if str(key) not in self._file.keys():
+                return {}
+            return self._file[str(key)].keys()
