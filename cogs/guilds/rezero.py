bb9d6aed229a760c6685ea7683d9357ee71e8d71
disneyresidents <disneyresidents@gmail.com>
2020-06-17 21:42:03 +0900

    [Fix]attachmentsをmap()でdiscord.Fileに変換してから送信するように変更
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/guilds/rezero.py
+++ cogs/guilds/rezero.py
@@ -7,15 +7,18 @@ class ReZero(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if message.author.bot:
+            return
         if message.guild is None:
             return
         if message.guild.id != 718784794422411354:
             return
         if message.attachments:
+            images = list(map(lambda x: x.to_file(), message.attachments))
             ch = self.bot.get_channel(718792795300692001)
             if message.channel == ch:
                 return
-            await ch.send(files=message.attachments)
+            await ch.send(files=images)
 
 
 def setup(bot):
