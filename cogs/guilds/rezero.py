f68b1fb23cf448635761cde471c055e2991effbb
disneyresidents <disneyresidents@gmail.com>
2020-06-17 23:18:15 +0900

    [Fix]リストの作り方をmap関数から内包表記に変更
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/guilds/rezero.py
+++ cogs/guilds/rezero.py
@@ -9,12 +9,15 @@ class ReZero(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
+
         if message.guild is None:
             return
+
         if message.guild.id != 718784794422411354:
             return
+
         if message.attachments:
-            images = list(map(lambda x: x.to_file(), message.attachments))
+            images = [await image.to_file() for image in message.attachments]
             ch = self.bot.get_channel(718792795300692001)
             if message.channel == ch:
                 return
