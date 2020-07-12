bea4cc47bbad4479dc98d7611bb9e2cde8bac79a
disneyresidents <disneyresidents@gmail.com>
2020-07-12 13:30:35 +0900

    [Rem]デバック用の例外処理を削除

--- cogs/events/loops.py
+++ cogs/events/loops.py
@@ -77,45 +77,42 @@ class Loops(commands.Cog):
 
     @tasks.loop(seconds=60)
     async def loop4(self):
-        try:
-            await self.bot.wait_until_ready()
-            hm = datetime.now().strftime("%H:%M")
-            if True:
-                ch = self.bot.get_channel(706322916060692571)
-                async for msg in ch.history():
-                    if msg.author == self.bot.user:
-                        last_record = await ch.fetch_message(msg.id)
-                        last_record_list = last_record.content.splitlines()
-                        break
+        await self.bot.wait_until_ready()
+        hm = datetime.now().strftime("%H:%M")
+        if True:
+            ch = self.bot.get_channel(706322916060692571)
+            async for msg in ch.history():
+                if msg.author == self.bot.user:
+                    last_record = await ch.fetch_message(msg.id)
+                    last_record_list = last_record.content.splitlines()
+                    break
 
-                mcid_uuid_dic = {
-                    "shibatanienn_ts": "f63f13d9-ea1d-43f9-a0c7-46bb9445625d",
-                    "takosan_ykz": "4303b357-30ca-4209-a6c9-d96bafc60cf0",
-                    "chorocra": "438ed7bf-cbcf-40d9-a672-aacc2868e267",
-                    "ranzumu": "45578816-9dab-49fc-bef0-0525e0a57289",
-                    "kakkoiihito": "24eeb1a3-ed4a-444e-828c-5318122f4e4a",
-                    "nekorobi_0": "d6be1561-47c1-4e67-9829-2aca48f9be39",
-                    "chorocra_19800": "10cf3d97-a6aa-4ef0-8c27-a917e5cb59d8",
-                    "kaerusan82433413": "9cec894e-9ae3-4a25-97c5-b7a6c55c1376"  # todo 8月になったら外す
-                }
+            mcid_uuid_dic = {
+                "shibatanienn_ts": "f63f13d9-ea1d-43f9-a0c7-46bb9445625d",
+                "takosan_ykz": "4303b357-30ca-4209-a6c9-d96bafc60cf0",
+                "chorocra": "438ed7bf-cbcf-40d9-a672-aacc2868e267",
+                "ranzumu": "45578816-9dab-49fc-bef0-0525e0a57289",
+                "kakkoiihito": "24eeb1a3-ed4a-444e-828c-5318122f4e4a",
+                "nekorobi_0": "d6be1561-47c1-4e67-9829-2aca48f9be39",
+                "chorocra_19800": "10cf3d97-a6aa-4ef0-8c27-a917e5cb59d8",
+                "kaerusan82433413": "9cec894e-9ae3-4a25-97c5-b7a6c55c1376"  # todo 8月になったら外す
+            }
 
-                msg = ""
-                for i, mcid in enumerate(mcid_uuid_dic.keys()):
-                    uuid = mcid_uuid_dic[mcid]
-                    if len(last_record_list) < i:
-                        data = self.bot.get_mined_block(uuid)
-                        msg += f"{mcid}の整地量>>>{data}(前日比:0)\n"
-                        continue
-                    last_user_record = int(re.sub(r'\(前日比:\d+\)', '', last_record_list[i].split('>>>')[1]))
-
+            msg = ""
+            for i, mcid in enumerate(mcid_uuid_dic.keys()):
+                uuid = mcid_uuid_dic[mcid]
+                if len(last_record_list) < i:
                     data = self.bot.get_mined_block(uuid)
+                    msg += f"{mcid}の整地量>>>{data}(前日比:0)\n"
+                    continue
+                last_user_record = int(re.sub(r'\(前日比:\d+\)', '', last_record_list[i].split('>>>')[1]))
 
-                    data_difference = data - last_user_record
-                    msg += f"{mcid}の整地量>>>{data}(前日比:{data_difference})\n"
-                    await asyncio.sleep(2)
-                await ch.send(msg)
-        except Exception as e:
-            print(e)
+                data = self.bot.get_mined_block(uuid)
+
+                data_difference = data - last_user_record
+                msg += f"{mcid}の整地量>>>{data}(前日比:{data_difference})\n"
+                await asyncio.sleep(2)
+            await ch.send(msg)
 
     @tasks.loop(seconds=60)
     async def data_upload(self):
