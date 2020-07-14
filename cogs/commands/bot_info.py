ac8a6fa24b0e93d6baff6918ca636765bebf6523
disneyresidents <disneyresidents@gmail.com>
2020-07-14 21:01:34 +0900

    [Change]bot_infoコマンドの仕様変更

--- cogs/commands/bot_info.py
+++ cogs/commands/bot_info.py
@@ -1,39 +1,55 @@
 import discord
 
 from discord.ext import commands
+import datetime
 
 
-class bot_info(commands.Cog):
+class BotInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="bot_info", enabled=False)
+    @commands.command(name="bot_info")
     async def _bot_info(self, ctx):
-        client = self.bot
+        async with ctx.channel.typing():
 
-        kazu = len(client.guilds)
-        guild_name = ""
-        for guild in client.guilds:
-            guild_name += f"{guild.name}、"
-        guild = f"このbotは{guild_name}以上{kazu}個のサーバに入っています"
+            role_count = len(ctx.guild.me.roles[1:])
+            role_name = "、".join(role.name for role in ctx.guild.me.roles[1:])
+            bot_role = f"{role_name}\nSum:{role_count}"
+            if role_count == 0:
+                bot_role = "まだ1つもroleを持っていません"
+            elif role_count >= 10:
+                bot_role = f"Sum:{role_count}"
 
-        role_name = ""
-        for role in ctx.guild.me.roles[1:]:
-            role_name += f"{role.name}、"
-        role_kazu = len(ctx.guild.me.roles[1:])
-        botRole = f"このbotは{role_name}以上{role_kazu}個のroleを持っています"
+            text = 0
+            voice = 0
+            for g in self.bot.guilds:
+                for ch in g.channels:
+                    if isinstance(ch, discord.TextChannel):
+                        text += 1
+                    elif isinstance(ch, discord.VoiceChannel):
+                        voice += 1
 
-        em_bot = discord.Embed(
-            title=f"Hi! I'm{client.user}!",
-            description="powered by discord.py\ndisneyresidents#8709 made me!")
-        em_bot.set_thumbnail(url=client.user.avatar_url)  # ユーザーアバターをセット
-        em_bot.add_field(name="Name", value=f"{client.user}")
-        em_bot.add_field(name="ID", value=client.user.id)
-        em_bot.add_field(name="Guilds", value=guild, inline=False)
-        em_bot.add_field(name="Roles", value=botRole, inline=False)
+            created_at_JST = self.bot.user.created_at + datetime.timedelta(hours=9)
 
-        await ctx.send(embed=em_bot)
+            owner = self.bot.get_user(544774774405201923)
+
+            embed = discord.Embed(
+                title=f"Hi! I'm{self.bot.user}!",
+                description=f"powered by discord.py\n{str(owner)} made me!\n"
+                            f"[Support Server]({self.bot.guild_invite_url})",
+                url=self.bot.invite_url)
+            print(self.bot.owner_id)
+            embed.set_thumbnail(url=self.bot.user.avatar_url)  # ユーザーアバターをセット
+            embed.add_field(name="Name", value=self.bot.user)
+            embed.add_field(name="ID", value=self.bot.user.id)
+            embed.add_field(name="Create_at", value=created_at_JST.strftime("%Y %m/%d %H:%M(JST)"))
+            embed.add_field(name="Guilds", value=f"{len(self.bot.guilds)}(Shared: {self.bot.get_shared_count(ctx.author)})")
+            embed.add_field(name="Roles", value=bot_role)
+            embed.add_field(name="Channels", value=f"total:{text + voice}\ntext:{text}\nvoice{voice}")
+            embed.add_field(name="Users", value=str(len(list(self.bot.get_all_members()))))
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
-    bot.add_cog(bot_info(bot))
+    bot.add_cog(BotInfo(bot))
