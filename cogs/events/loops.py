694515fb0e20e2f41c306acbcd43a880a60b54e0
disneyresidents <disneyresidents@gmail.com>
2020-08-06 19:45:55 +0900

    [Del]マグロ鯖での機能を削除

--- cogs/events/loops.py
+++ cogs/events/loops.py
@@ -57,16 +57,6 @@ class Loops(commands.Cog):
             data = cur.fetchone()
             await ch.send(f"```{data[0]}```")
 
-        if hm == "19:00":
-            guild = client.get_guild(610309046851076121)
-            role = discord.utils.get(guild.roles, id=665853511740948500)  # 新規
-            channel = client.get_channel(665854731180310528)
-            kazu = len(role.members)
-            if kazu == 0:
-                await channel.send("おっと、始めにを読んでない人はいないみたいです")
-            else:
-                await channel.send(f"{role.mention}\n{kazu}人の人がまだ<#630402461395451913>を読んでないみたいですね")
-
         if hm == "23:50":
             ch = self.bot.get_channel(706322916060692571)
             async for msg in ch.history():
