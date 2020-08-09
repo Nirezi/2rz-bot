f64efc0ca39ef8211b3b53ebf25940f9cd0c3b14
disneyresidents <disneyresidents@gmail.com>
2020-08-09 16:31:25 +0900

    [Add]いろはサーバでの機能追加

---
+++ cogs/guilds/iroha.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+import paramiko
+import os
+
+SSH = paramiko.SSHClient()
+
+host = os.environ["Host"]
+port = os.environ["Port"]
+user = os.environ["User"]
+Key = "~/.ssh/id_rsa"
+
+
+class Iroha(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        SSH.connect(host, int(port), user, key_filename=Key)
+
+    def cog_check(self, ctx):
+        return ctx.author.guild_permissions.administrator
+
+    @commands.command(aliases=["boot"])
+    async def start(self, ctx):
+        SSH.exec_command("cd ~/Minecraft && bash start.sh")
+
+    @commands.command()
+    async def stop(self, ctx):
+        SSH.exec_command("cd ~/Minecraft && bash stop.sh")
+
+    @commands.command(aliases=["reboot"])
+    async def restart(self, ctx):
+        SSH.exec_command("cd ~/Minecraft && bash restart.sh")
+
+
+def setup(bot):
+    bot.add_cog(Iroha(bot))
