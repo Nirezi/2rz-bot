ea698895f35f570cf39550541e1a43e020448e1d
disneyresidents <disneyresidents@gmail.com>
2020-08-11 01:36:33 +0900

    [Add]コマンドを打った際のメッセージを追加、evalコマンドを追加
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/guilds/iroha.py
+++ cogs/guilds/iroha.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import paramiko
 import os
+import asyncio
 
 SSH = paramiko.SSHClient()
 
@@ -14,6 +15,7 @@ SSH.load_host_keys(f"/home/{user}/.ssh/known_hosts")
 class Iroha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.loop = asyncio.get_event_loop()
         SSH.connect(host, int(port), user, key_filename=Key)
 
     def cog_check(self, ctx):
@@ -28,15 +30,34 @@ class Iroha(commands.Cog):
 
     @commands.command(aliases=["boot"])
     async def start(self, ctx):
-        SSH.exec_command("cd ~/Minecraft && bash start.sh")
+        stdin, stdout, stderr = self.loop.run_in_executor(None, SSH.exec_command, "screen -ls | grep minecraft")
+        if stdin:
+            return await ctx.send("Server is already running!")
+        await ctx.send("Starting minecraft server,,")
+        self.loop.run_in_executor(None, SSH.exec_command, "cd ~/Minecraft && bash start.sh")
 
-    @commands.command()
+    @commands.command(aliases=["kill"])
     async def stop(self, ctx):
-        SSH.exec_command("cd ~/Minecraft && bash stop.sh")
+        stdin, stdout, stderr = self.loop.run_in_executor(None, SSH.exec_command, "screen -ls | grep minecraft")
+        if not stdin:
+            return await ctx.send("Server isn't running yet!")
+        await ctx.send("Stopping minecraft server,,")
+        self.loop.run_in_executor(None, SSH.exec_command, "cd ~/Minecraft && bash stop.sh")
 
     @commands.command(aliases=["reboot"])
     async def restart(self, ctx):
-        SSH.exec_command("cd ~/Minecraft && bash restart.sh")
+        stdin, stdout, stderr = self.loop.run_in_executor(None, SSH.exec_command, "screen -ls | grep minecraft")
+        if not stdin:
+            return await ctx.send("Server isn't running yet!")
+        ch = self.bot.get_channel(739270726036488272)
+        await ch.send(":warning:Warning!10秒後にサーバーが再起動するよ！")
+        await asyncio.sleep(10)
+        self.loop.run_in_executor(None, SSH.exec_command, "cd ~/Minecraft && bash restart.sh")
+
+    @commands.command(name="eval")
+    async def _eval(self, ctx, *, command):
+        self.loop.run_until_complete(SSH.exec_command(command))
+        await ctx.send(f"Command {command} has done by {ctx.author.name}")
 
 
 def setup(bot):
