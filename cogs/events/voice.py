8a8cceee0b139736f4a4cf818175c428d98b7c13
disneyresidents <disneyresidents@gmail.com>
2020-08-11 01:19:38 +0900

    [Add]voice set_logコマンドを追加
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/events/voice.py
+++ cogs/events/voice.py
@@ -1,3 +1,7 @@
+import asyncio
+import re
+
+import discord
 from discord.ext import commands
 
 
@@ -23,13 +27,52 @@ class Voice(commands.Cog):
                 ch = self.bot.get_channel(ch_dic[before.channel.id])
                 await ch.send(f"{member}が{before.channel}にから離脱しました")
 
-            #if after.channel and before.channel:
-            #    if after.channel.id not in ch_dic.keys():
-            #        return
-            #    after_ch = self.bot.get_channel(ch_dic[after.channel.id])
-            #    before_ch = self.bot.get_channel(ch_dic[before.channel.id])
-            #    await after_ch.send(f"{member}が{before.channel}から{after.channel}に移動してきました")
-            #    await before_ch.send(f"{member}が{before.channel}から{after.channel}に移動しました")
+    @commands.has_permissions(manage_guild=True)
+    @commands.group(invoke_without_command=True)
+    async def voice(self, ctx):
+        await ctx.send(f"{ctx.prefix}voice [set_log, rem_log]")
+
+    @commands.has_permissions(manage_guild=True)
+    @voice.command()
+    async def set_log(self, ctx, ch_id: int = None):
+        if ctx.author.voice.channel is None or ch_id is None:
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            await ctx.send("監視するボイスチャンネルのidまたは名前を送信してください")
+
+            try:
+                msg = await self.bot.wait_for("message", check=check, timeout=300)
+            except asyncio.TimeoutError:
+                return await ctx.send("タイムアウトしました。最初からやり直してください")
+
+            p = re.compile(r"[0-9]+")
+            if p.fullmatch(msg.content):
+                ch = self.bot.get_channel(int(msg.content))
+            else:
+                ch = discord.utils.get(ctx.guild.voice_channels, name=msg.content)
+
+            if ch is None:
+                return await ctx.send("チャンネルを見つけられませんでした、名前、idが間違っていないか確かめてください")
+
+            if not isinstance(ch, discord.VoiceChannel):
+                return await ctx.send("指定されたチャンネルはボイスチャンネルではありません。名前、idが間違っていないか確かめてください")
+
+            await self.bot.voice_log.put(ch.id, ctx.channel.id)
+            await ctx.send(f"{ctx.channel}を{ch}のログチャンネルとして設定しました")
+        elif ctx.author.voice.channel:
+            await self.bot.voice_log.put(ctx.author.voice.channel.id, ctx.channel.id)
+            await ctx.send(f"{ctx.channel}を{ctx.author.voice.channel}のログチャンネルとして設定しました")
+        elif ch_id:
+            ch = self.bot.get_channel(ch_id)
+            if ch is None:
+                return await ctx.send("チャンネルを見つけられませんでした、名前、idが間違っていないか確かめてください")
+
+            if not isinstance(ch, discord.VoiceChannel):
+                return await ctx.send("指定されたチャンネルはボイスチャンネルではありません。名前、idが間違っていないか確かめてください")
+
+
+
 
 
 def setup(bot):
