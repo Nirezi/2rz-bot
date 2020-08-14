1815f15f1fd40d1a51d56f9336cff0626b0b2a6d
disneyresidents <disneyresidents@gmail.com>
2020-08-14 15:56:43 +0900

    [Change]コマンドの定義位置を変更
    Signed-off-by: disneyresidents <disneyresidents@gmail.com>

--- cogs/guilds/nirezi.py
+++ cogs/guilds/nirezi.py
@@ -16,23 +16,6 @@ class Nirezi(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def cog_check(self, ctx):
-        if ctx.guild is None:
-            return False
-        elif ctx.guld.id == 621326525521723414:
-            return True
-        else:
-            return False
-
-    @commands.command(name="join")
-    @commands.has_role(672006791474708490)
-    async def _join(self, ctx):
-        role = discord.utils.get(ctx.guild.roles, id=629828134820380682)
-        if role in ctx.author.roles:
-            return await ctx.send("(´・ω・｀)もう参加してるじゃん")
-        await ctx.author.add_roles(role)
-        await ctx.send("役職を付与しました！発表をお楽しみに！")
-
     @commands.Cog.listener()
     async def on_message(self, message):
         client = self.bot
@@ -171,7 +154,6 @@ class Nirezi(commands.Cog):
 
                 msg = await client.wait_for("message", check=check)
                 await mcs("hello!")
-            await self.bot.process_commands(message)
 
 
 def setup(bot):
