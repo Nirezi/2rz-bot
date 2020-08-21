590047072e623f78aac763660cfb898b46b2c8a9
disneyresidents <disneyresidents@gmail.com>
2020-08-21 23:28:01 +0900

    [Change]コマンドをまとめる

--- cogs/commands/meta.py
+++ cogs/commands/meta.py
@@ -1,11 +1,91 @@
+import asyncio
+import datetime
+
 import discord
 from discord.ext import commands
-import asyncio
 
 
-class CheckPermission(commands.Cog):
+class Meta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name="bot_info")
+    async def _bot_info(self, ctx):
+        async with ctx.channel.typing():
+
+            role_count = len(ctx.guild.me.roles[1:])
+            role_name = "、".join(role.name for role in ctx.guild.me.roles[1:])
+            bot_role = f"{role_name}\nSum:{role_count}"
+            if role_count == 0:
+                bot_role = "まだ1つもroleを持っていません"
+            elif role_count >= 10:
+                bot_role = f"Sum:{role_count}"
+
+            text = 0
+            voice = 0
+            for g in self.bot.guilds:
+                for ch in g.channels:
+                    if isinstance(ch, discord.TextChannel):
+                        text += 1
+                    elif isinstance(ch, discord.VoiceChannel):
+                        voice += 1
+
+            bot = 0
+            user = 0
+            for m in self.bot.get_all_members():
+                if m.bot:
+                    bot += 1
+                else:
+                    user += 1
+
+            created_at_JST = self.bot.user.created_at + datetime.timedelta(hours=9)
+
+            owner = self.bot.get_user(544774774405201923)
+
+            embed = discord.Embed(
+                title=f"Hi! I'm{self.bot.user}!",
+                description=f"powered by discord.py\n{str(owner)} made me!\n"
+                            f"[Support Server]({self.bot.guild_invite_url})",
+                url=self.bot.invite_url)
+            print(self.bot.owner_id)
+            embed.set_thumbnail(url=self.bot.user.avatar_url)  # ユーザーアバターをセット
+            embed.add_field(name="Name", value=self.bot.user)
+            embed.add_field(name="ID", value=self.bot.user.id)
+            embed.add_field(name="Create_at", value=created_at_JST.strftime("%Y %m/%d %H:%M(JST)"))
+            embed.add_field(name="Guilds", value=f"{len(self.bot.guilds)}(Shared: {self.bot.get_shared_count(ctx.author)})")
+            embed.add_field(name="Roles", value=bot_role)
+            embed.add_field(name="Channels", value=f"total:{text + voice}\ntext:{text}\nvoice:{voice}")
+            embed.add_field(name="Users", value=f"total:{user + bot}\nuser:{user}\nbot:{bot}")
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="avaatr")
+    async def _avatar(self, ctx, id: int = None):
+        if id is None:
+            await ctx.send("idを指定してね！")
+            return
+
+        user = self.bot.get_user(id)
+        guild = self.bot.get_guild(id)
+
+        if user is not None:
+            embed = discord.Embed(title=f"{user}", description="")
+            embed.set_image(url=user.avatar_url)
+            await ctx.send(embed=embed)
+        elif guild is not None:
+            if len(guild.icon_url) == 0:
+                await ctx.send("標準のアイコンだから表示できないや！")
+            else:
+                embed = discord.Embed(title=f"{guild}", description="")
+                embed.set_image(url=guild.icon_url)
+                await ctx.send(embed=embed)
+        else:
+            await ctx.send("404 NotFound\nあれ？ちょっと僕には見つけられそうにないや、、")
+
+    @_avatar.error
+    async def avatar_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("もしかして:idが数字じゃない")
 
     @commands.command(aliases=["check_per", "cp"])
     @commands.guild_only()
@@ -126,4 +206,4 @@ class CheckPermission(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(CheckPermission(bot))
+    bot.add_cog(Meta(bot))
