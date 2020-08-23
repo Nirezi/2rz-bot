3a4fd8c13400c720850f5d9419117006b7ded6e8
disneyresidents <disneyresidents@gmail.com>
2020-08-23 16:24:31 +0900

    [Add]企画の当選発表機能を追加

--- cogs/commands/event.py
+++ cogs/commands/event.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import random
 
 
 class Event(commands.Cog):
@@ -22,6 +23,34 @@ class Event(commands.Cog):
             return await ctx.send("(´・ω・｀)もう参加してるじゃん")
         await ctx.author.add_roles(role)
         await ctx.send("役職を付与しました！発表をお楽しみに！")
+
+    @commands.command()
+    @commands.has_role(621326896554311700)
+    async def choice(self, ctx):
+        async with ctx.typing():
+            role = discord.utils.get(ctx.guild.roles, id=629828134820380682)
+            winner_role = discord.utils.get(ctx.guild.roles, id=746988608728072293)
+            winners = random.sample(role.members, 16)
+            embed = discord.Embed(title="当選発表！")
+            text = ""
+            for i, user in enumerate(winners):
+                if i == 0:
+                    text += "2st(1)\n"
+                elif i == 1:
+                    text += "1st(1)\n"
+                elif i == 2:
+                    text += "32(2)\n"
+                elif i == 4:
+                    text += "16(4)\n"
+                elif i == 8:
+                    text += "8(8)\n"
+                text += f"{user.mention}\n"
+                await user.add_roles(winner_role)
+            else:
+                text += "参加賞\nその他の方"
+            embed.add_field(name="当選者", value=text)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
