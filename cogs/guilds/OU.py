be88b0d00115e25565edc4e046c92fa0b5c5f401
disneyresidents <disneyresidents@gmail.com>
2020-09-03 17:52:03 +0900

    [Add]運営連での機能追加

---
+++ cogs/guilds/OU.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+
+
+class OU(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if message.guild.id != 675314750783094806:
+            return
+        if message.channel.id == 750997955145629796:
+            reacts = ["\U0001f44d", "\U0001f44e"]
+            for react in reacts:
+                await message.add_reaction(react)
+
+
+def setup(bot):
+    bot.add_cog(Ou(bot))
