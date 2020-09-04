b024e9b3e0a6f2d5fe32f70c20d41a9238a3c604
disneyresidents <disneyresidents@gmail.com>
2020-09-05 00:32:22 +0900

    [Change]コマンドをまとめる

---
+++ cogs/commands/guild.py
@@ -0,0 +1,93 @@
+from discord.ext import commands
+import discord
+import asyncio
+
+
+class Guild(commands.Cog):
+    """guildに関係するcog"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.reacts = [
+            "\N{BLACK LEFT-POINTING TRIANGLE}",  # 戻る
+            "\N{BLACK RIGHT-POINTING TRIANGLE}"  # 進む
+        ]
+
+    def cog_check(self, ctx):
+        if not ctx.guild:
+            return False
+        return True
+
+    async def check_page(self, ctx, msg: discord.Message, page: int, max_page: int) -> int:
+        def check(reaction, user):
+            if user.bot or user.id != ctx.author.id:
+                return
+            elif reaction.message.id != msg.id:
+                return False
+            elif str(reaction.emoji) in self.reacts:
+                return True
+            else:
+                return False
+
+        try:
+            reaction, user = await self.bot.wait_for("reaction_add", check=check, timeout=300)
+        except asyncio.TimeoutError:
+            return await msg.clear_reactions()
+        else:
+            emoji = str(reaction.emoji)
+            if emoji == self.reacts[0]:
+                if page == 0:
+                    return max_page
+                return page - 1
+            else:
+                if page == max_page:
+                    return 0
+                return page + 1
+
+    @commands.command()
+    async def myrole(self, ctx):
+        """コマンドの実行者が持っているroleを表示"""
+        if (count := len(ctx.author.roles[1:])) == 0:
+            await ctx.send("おっと、まだroleを持っていないみたいですね")
+        else:
+            if count >= 20:
+                roles = "、".join(r.name for r in ctx.author.roles[1:22])
+                embed = discord.Embed(title="roleは以下の通りです!(1ページ目)", description=roles)
+                msg = await ctx.send(embed=embed)
+                page = 0
+                max_page = -(-len(ctx.author.roles[1:]) // 20)
+
+                while not self.bot.is_closed():
+                    page = await self.check_page(ctx, msg, page, max_page)
+                    roles = "、".join(r.name for r in ctx.author.roles[1 + 20 * page: page * 20 + 22])
+                    embed = discord.Embed(title=f"roleは以下の通りです!({page}ページ目)", description=roles)
+                    await msg.edit(embed=embed)
+            else:
+                roles = "、".join(r.name for r in ctx.author.roles[1:])
+
+                embed = discord.Embed(title="roleは以下の通りです！", description=f"{roles}\nSum:{count}")
+                await ctx.send(embed=embed)
+
+    @commands.command()
+    async def members(self, ctx):
+        """実行したサーバーのユーザーを表示"""
+        if (count := len(ctx.guild.members)) >= 100:
+            members = "、".join(r.name for r in ctx.guild.members[:101])
+            embed = discord.Embed(title="メンバーは以下の通りです!(1ページ目)", description=members)
+            msg = await ctx.send(embed=embed)
+            page = 0
+            max_page = -(-len(ctx.guild.roles) // 20)
+
+            while not self.bot.is_closed():
+                page = await self.check_page(ctx, msg, page, max_page)
+                members = "、".join(m.name for m in ctx.guild.members[:page * 100 + 101])
+                embed = discord.Embed(title=f"メンバーは以下の通りです!({page}ページ目)", description=members)
+                await msg.edit(embed=embed)
+        else:
+            members = "、".join(m.name for m in ctx.guild.members)
+
+            embed = discord.Embed(title="メンバーは以下の通りです！", description=f"{members}\nSum:{count}")
+            await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Guild(bot))
--- cogs/commands/simple_commands.py
+++ cogs/commands/simple_commands.py
@@ -1,12 +1,5 @@
-import sys
-
 import discord
 from discord.ext import commands
-
-from citycodes_dic import citycodes_dic
-from def_list import wait_for_react
-
-sys.path.append("../")
 
 
 class SimpleCommands(commands.Cog):
@@ -18,20 +11,6 @@ class SimpleCommands(commands.Cog):
         return ctx.message.content == ctx.prefix + ctx.invoked_with
 
     @commands.command()
-    async def myrole(self, ctx):
-        """コマンドの実行者が持っているroleを表示"""
-        if (count := len(ctx.author.roles[1:])) == 0:
-            await ctx.send("おっと、まだroleを持っていないみたいですね？")
-        else:
-            roles_name = "、".join(r.name for r in ctx.author.roles[1:])
-
-            embed = discord.Embed(title="", description=f"あなたは{count}個のroleを持っています\n詳細を表示するにはリアクションを押してください")
-            msg = await ctx.send(embed=embed)
-            embed2 = discord.Embed(title="あなたのroleは以下の通りです", description=roles_name)
-
-            await wait_for_react(self.bot, ctx, msg, embed2)
-
-    @commands.command()
     async def dm(self, ctx):
         """dmに凸する"""
         msg_list = ["な", "に", "か", "よ", "う", "か", "な", "？"]
@@ -40,20 +19,6 @@ class SimpleCommands(commands.Cog):
                 await ctx.author.send(msg)
         except discord.Forbidden:
             await ctx.send(f"{ctx.author.mention}さてはdmが送れない設定にしてるな？")
-
-    @commands.command()
-    async def members(self, ctx):
-        """実行したサーバーのユーザーを表示"""
-        members_name = ""
-        for member in ctx.guild.members:
-            members_name += member.name
-        mem_count = len(ctx.guild.members)
-
-        embed = discord.Embed(title="", description=f"この鯖には{mem_count}人のユーザーがいます\n詳細を表示するにはリアクションを押してください")
-        msg = await ctx.send(embed=embed)
-        embed2 = discord.Embed(title="この鯖のメンバーは以下の通りです", description=members_name)
-
-        await wait_for_react(self.bot, ctx, msg, embed2)
 
     @commands.command()
     async def invite(self, ctx):
