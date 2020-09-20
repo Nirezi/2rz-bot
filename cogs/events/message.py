79da6e27536e6ac70c99991bf6ba783b485303d1
disneyresidents <disneyresidents@gmail.com>
2020-09-20 17:19:54 +0900

    [Del]メンションされた時の反応を無くす

--- cogs/events/message.py
+++ cogs/events/message.py
@@ -10,8 +10,6 @@ class Message(commands.Cog, name="message"):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        mention = message.author.mention
-
         if message.author.bot:  # botのメッセージなら無視する
             return
 
@@ -20,18 +18,6 @@ class Message(commands.Cog, name="message"):
 
         if message.content.startswith("!"):
             return
-
-        # now = datetime.now()
-        mcs = message.channel.send
-        client = self.bot
-
-        if client.user in message.mentions:  # メンションの感知
-            if message.guild is None:
-                prefix = "/"
-            else:
-                prefix = self.bot.prefixes.get(message.guild.id, "/")
-            msg = f"{mention}呼んだ？\nヘルプは {prefix}helpです。"
-            await mcs(msg)
 
         if re.findall(r"https://(?:ptb.|canary.)?discord(?:app)?.com/channels/[0-9]+/[0-9]+/[0-9]+", message.content) and ("@" not in message.content):
             if str(message.guild.id) in self.bot.settings.keys('not_quote'):
