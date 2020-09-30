9680c8a67995332ff42a2cd81f2c4eeebd4d7aab
disneyresidents <disneyresidents@gmail.com>
2020-09-30 15:55:15 +0900

    [Change]helpコマンドの仕様変更

--- cogs/commands/manage_help.py
+++ cogs/commands/manage_help.py
@@ -4,9 +4,9 @@ import discord
 from discord.ext import commands
 
 
-class Help(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class BotHelp(commands.HelpCommand):
+    def __init__(self):
+        super().__init__()
 
     @staticmethod
     def get_aliase(command: commands.core.Command) -> str:
@@ -22,43 +22,40 @@ class Help(commands.Cog):
         subcommands = "/".join(c.name for c in parent.commands)
         return subcommands
 
-    @commands.command(name="help")
-    @commands.bot_has_permissions(add_reactions=True, manage_messages=True)
-    async def _help(self, ctx, cmd=None):
-        """ヘルプを送信"""
-        if cmd is not None:
-            command = self.bot.get_command(cmd)
-            if command is None:
-                return await ctx.send(f"おっと、`{cmd}`は見つかりません！")
+    def command_not_found(self, string):
+        return f"おっと、{string}というコマンドはありません！"
 
-        cog_dic = dict(self.bot.cogs).copy()
-        cog_dic.pop("Jishaku")
-        cogs = [v for k, v in cog_dic.items() if v.qualified_name[0].isupper()]
-        max_page = len(cogs)
+    def subcommand_not_found(self, command, string):
+        if isinstance(command, commands.Group):
+            return f"おっと、{string}というサブコマンドはありません！"
+        return f"{command.name}にサブコマンドはありません！"
 
-        count = 0
-        for cog in cogs:
-            for cmd in cog.get_commands():
-                if isinstance(cmd, commands.core.Group):
-                    count += len(cmd.commands)
-                else:
-                    count += 1
+    async def send_bot_help(self, mapping):
+        bot = self.context.bot
+        entries = await self.filter_commands(bot.commands, sort=True)
+        all_commands = {}
+        for cmd in entries:
+            if cmd.cog is None:
+                continue
+            try:
+                all_commands[cmd.cog].append(cmd)
+            except KeyError:
+                all_commands[cmd.cog] = [cmd]
 
-        def page_setup(page: int) -> discord.Embed:
+        max_page = len(all_commands.keys())
+        count = len(all_commands.items())
+        page = 1
+
+        def page_setup(cog: commands.Cog) -> discord.Embed:
             """ページ数に対応したhelp内容をセット"""
-            page -= 1
-            embed = discord.Embed(title=f"Page {page+1}/{max_page} ({count} commands)",
-                                  description=f"より詳細なヘルプは[公式サーバ]({self.bot.guild_invite_url})まで!")
-            cog = cogs[page]
+            embed = discord.Embed(title=f"Page {page}/{max_page} ({count} commands)",
+                                  description=f"より詳細なヘルプは[公式サーバ]({bot.guild_invite_url})まで!")
             embed.add_field(name=cog.qualified_name, value=cog.description)
-
-            for cmd in cog.get_commands():
-                if cmd.hidden or not cmd.enabled:
-                    continue
+            for cmd in all_commands[cog]:
                 if cmd.aliases:
-                    embed.add_field(name=f"{cmd.name} {self.get_aliase(cmd)}", value=cmd.help, inline=False)
+                    embed.add_field(name=f"{cmd.name} {self.get_aliase(cmd)}", value=cmd.short_doc, inline=False)
                 else:
-                    embed.add_field(name=f"{cmd.name}", value=cmd.help, inline=False)
+                    embed.add_field(name=f"{cmd.name}", value=cmd.short_doc, inline=False)
             return embed
 
         react_list = [
@@ -69,27 +66,25 @@ class Help(commands.Cog):
             "\N{BLACK SQUARE FOR STOP}\N{VARIATION SELECTOR-16}"  # stop
         ]
 
-        page = 1
-        msg = await ctx.send(embed=page_setup(page))
-
+        msg = await self.context.send(embed=page_setup(list(all_commands.keys())[page-1]))
         for react in react_list:
             await msg.add_reaction(react)  # リアクション付与
 
         def check(reaction, user):
             if reaction.message.id != msg.id:
                 return False
-            elif ctx.author.bot or user != ctx.author:
+            elif self.context.author.bot or user != self.context.author:
                 return False
             elif str(reaction.emoji) in react_list:
                 return True
             else:
                 return False
 
-        while not self.bot.is_closed():
+        while not bot.is_closed():
             try:
-                react, user = await self.bot.wait_for("reaction_add", check=check, timeout=300)
+                react, user = await bot.wait_for("reaction_add", check=check, timeout=300)
             except asyncio.TimeoutError:
-                await ctx.message.clear_reactions()
+                await msg.clear_reactions()
                 break
             else:
                 try:
@@ -101,37 +96,30 @@ class Help(commands.Cog):
                                 page = max_page
                             else:
                                 page -= 1
-
                         if emoji == u"\u25B6":  # 進む
                             if page == max_page:
                                 page = 1
                             else:
                                 page += 1
-
-                        await msg.edit(embed=page_setup(page))
-
+                        await msg.edit(embed=page_setup(list(all_commands.keys())[page-1]))
                     if emoji == "\U0001f522":  # 1234
-                        def check_msg(m):
-                            return m.author == ctx.author and m.channel == ctx.channel
-
-                        await ctx.send("移動したいページ数を送信してください！", delete_after=30)
+                        await self.context.send("移動したいページ数を送信してください！", delete_after=30)
                         try:
-                            m = await self.bot.wait_for("message", check=check_msg, timeout=30)
+                            m = await bot.wait_for("message", check=lambda m: m.author == self.context.author, timeout=30)
                             next_page = int(m.content)
                             await m.delete()
                         except asyncio.TimeoutError:
                             await msg.clear_reactions()
                             break
                         except ValueError:
-                            await ctx.send("おっと、ページ数が整数じゃないみたいです", delete_after=5)
+                            await self.context.send("おっと、ページ数が整数じゃないみたいです", delete_after=5)
                             continue
                         else:
                             if not(1 <= next_page <= max_page):
-                                await ctx.send(f"おっと、{next_page}ページはありません！", delete_after=5)
+                                await self.context.send(f"おっと、{next_page}ページはありません！", delete_after=5)
                                 continue
                             page = next_page
-                            await msg.edit(embed=page_setup(page))
-
+                            await msg.edit(embed=page_setup(list(all_commands.keys())[page-1]))
                     if emoji == "\U00002139\U0000fe0f":  # iマーク
                         embed = discord.Embed(title="インフォメーション")
                         embed.add_field(name="各種リアクションのヘルプ",
@@ -142,16 +130,23 @@ class Help(commands.Cog):
                                               f"{react_list[4]}:メッセージを削除します\n")
                         await msg.edit(embed=embed)
                         await asyncio.sleep(10)
-                        await msg.edit(embed=page_setup(page))
-
+                        await msg.edit(embed=page_setup(list(all_commands.keys())[page-1]))
                     if emoji == "\N{BLACK SQUARE FOR STOP}\N{VARIATION SELECTOR-16}":  # ■
                         await msg.delete()
                         break
-
                 except IndexError:
-                    await ctx.send("範囲外のリアクションが押されました", delete_after=3.0)
+                    await self.context.send("範囲外のリアクションが押されました", delete_after=3.0)
                     continue
+
+    async def send_cog_help(self, cog):
+        pass
+
+    async def send_command_help(self, command):
+        pass
+
+    async def send_group_help(self, group):
+        pass
 
 
 def setup(bot):
-    bot.add_cog(Help(bot))
+    pass
