01767f7bea8f38ab78b9b47869a66cb284cf1ac8
disneyresidents <disneyresidents@gmail.com>
2020-10-09 00:00:40 +0900

    [Change]ヘルプコマンドの動作を変更する

--- cogs/commands/manage_help.py
+++ cogs/commands/manage_help.py
@@ -32,9 +32,7 @@ class BotHelp(commands.HelpCommand):
         return f"おっと、{string}というコマンドはありません！"
 
     def subcommand_not_found(self, command, string):
-        if isinstance(command, commands.Group):
-            return f"おっと、{string}というサブコマンドはありません！"
-        return f"{command.name}にサブコマンドはありません！"
+        return f"おっと、{command}に{string}というサブコマンドはありません！"
 
     async def send_bot_help(self, mapping):
         can_remove_emoji = self.check_perm()
@@ -44,12 +42,11 @@ class BotHelp(commands.HelpCommand):
         for cmd in entries:
             if cmd.cog is None:
                 continue
-            elif cmd.cog.qualified_name == "Jishaku":
-                continue
-            try:
-                all_commands[cmd.cog].append(cmd)
-            except KeyError:
-                all_commands[cmd.cog] = [cmd]
+            elif cmd.cog.qualified_name != "Jishaku":
+                try:
+                    all_commands[cmd.cog].append(cmd)
+                except KeyError:
+                    all_commands[cmd.cog] = [cmd]
 
         max_page = len(all_commands.keys())
         count = 0
@@ -98,53 +95,55 @@ class BotHelp(commands.HelpCommand):
                 emoji = str(react.emoji)
                 if can_remove_emoji:
                     await msg.remove_reaction(emoji, user)
-                    if emoji == u"\u25C0" or emoji == u"\u25B6":  # 進むか戻る
-                        if emoji == u"\u25C0":  # 戻る
-                            if page == 1:
-                                page = max_page
-                            else:
-                                page -= 1
-                        if emoji == u"\u25B6":  # 進む
-                            if page == max_page:
-                                page = 1
-                            else:
-                                page += 1
-                        await msg.edit(embed=page_setup(list(all_commands.keys())[page - 1]))
-                    if emoji == "\U0001f522":  # 1234
-                        await self.context.send("移動したいページ数を送信してください！", delete_after=30)
-                        try:
-                            m = await bot.wait_for("message", check=lambda m: m.author == self.context.author, timeout=30)
-                            next_page = int(m.content)
-                            await m.delete()
-                        except asyncio.TimeoutError:
-                            await msg.clear_reactions()
-                            break
-                        except ValueError:
-                            await self.context.send("おっと、ページ数が整数じゃないみたいです", delete_after=5)
-                            continue
+                if emoji == u"\u25C0" or emoji == u"\u25B6":  # 進むか戻る
+                    if emoji == u"\u25C0":  # 戻る
+                        if page == 1:
+                            page = max_page
                         else:
-                            if not(1 <= next_page <= max_page):
-                                await self.context.send(f"おっと、{next_page}ページはありません！", delete_after=5)
-                                continue
-                            page = next_page
-                            await msg.edit(embed=page_setup(list(all_commands.keys())[page - 1]))
-                    if emoji == "\U00002139\U0000fe0f":  # iマーク
-                        embed = discord.Embed(title="インフォメーション")
-                        embed.add_field(name="各種リアクションのヘルプ",
-                                        value=f"{react_list[0]}:一つ前のページに戻ります。最初のページで使用すると最後のページに移動します。\n\n"
-                                              f"{react_list[1]}:一つ後ろのページに移動します。最後のページで使用すると最初のページに移動します。\n\n"
-                                              f"{react_list[2]}:botのメッセージの後に、移動したいページを送信するとそのページに移動できます。\n\n"
-                                              f"{react_list[3]}:このヘルプを表示します。10秒後に元のヘルプに自動的に戻ります。\n\n"
-                                              f"{react_list[4]}:メッセージを削除します\n")
-                        await msg.edit(embed=embed)
-                        await asyncio.sleep(10)
-                        await msg.edit(embed=page_setup(list(all_commands.keys())[page - 1]))
-                    if emoji == "\N{BLACK SQUARE FOR STOP}\N{VARIATION SELECTOR-16}":  # ■
-                        await msg.delete()
+                            page -= 1
+                    if emoji == u"\u25B6":  # 進む
+                        if page == max_page:
+                            page = 1
+                        else:
+                            page += 1
+                    await msg.edit(embed=page_setup(list(all_commands.keys())[page - 1]))
+
+                if emoji == "\U0001f522":  # 1234
+                    ask_page = await self.context.send("移動したいページ数を送信してください！", delete_after=30)
+                    try:
+                        m = await bot.wait_for("message", check=lambda m: m.author == self.context.author, timeout=30)
+                        next_page = int(m.content)
+                        await m.delete()
+                    except asyncio.TimeoutError:
+                        if can_remove_emoji:
+                            await msg.clear_reactions()
                         break
-                except IndexError:
-                    await self.context.send("範囲外のリアクションが押されました", delete_after=3.0)
-                    continue
+                    except ValueError:
+                        await self.context.send("おっと、ページ数が整数じゃないみたいです", delete_after=5)
+                        continue
+                    else:
+                        if not(1 <= next_page <= max_page):
+                            await self.context.send(f"おっと、{next_page}ページはありません！", delete_after=5)
+                            continue
+                        page = next_page
+                        await msg.edit(embed=page_setup(list(all_commands.keys())[page - 1]))
+                    finally:
+                        await ask_page.delete()
+
+                if emoji == "\U00002139\U0000fe0f":  # iマーク
+                    embed = discord.Embed(title="インフォメーション")
+                    embed.add_field(name="各種リアクションのヘルプ",
+                                    value=f"{react_list[0]}:一つ前のページに戻ります。最初のページで使用すると最後のページに移動します。\n\n"
+                                          f"{react_list[1]}:一つ後ろのページに移動します。最後のページで使用すると最初のページに移動します。\n\n"
+                                          f"{react_list[2]}:botのメッセージの後に、移動したいページを送信するとそのページに移動できます。\n\n"
+                                          f"{react_list[3]}:このヘルプを表示します。10秒後に元のヘルプに自動的に戻ります。\n\n"
+                                          f"{react_list[4]}:メッセージを削除します\n")
+                    await msg.edit(embed=embed)
+                    await asyncio.sleep(10)
+                    await msg.edit(embed=page_setup(list(all_commands.keys())[page - 1]))
+                if emoji == "\N{BLACK SQUARE FOR STOP}\N{VARIATION SELECTOR-16}":  # ■
+                    await msg.delete()
+                    break
 
     async def send_cog_help(self, cog):
         pass
