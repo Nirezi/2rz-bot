8aaa27dc5ccc3825c2928b86d6518e9f74bddeb3
disneyresidents <disneyresidents@gmail.com>
2020-11-16 10:09:17 +0900

    [Add]スレッド的な機能を追加する

---
+++ cogs/guilds/NIT.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+class NIT(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.guild is None:
+            return
+
+        if message.channel.id == 776641961854763058:
+            category = self.bot.get_channel(776641214681841686)
+            ch = await message.guild.create_text_channel(name=message.content, category=category)
+            embed = discord.Embed(description=f"{message.author.mention}->{ch.mention}を作成しました")
+            await message.channel.send(embed=embed)
+
+        if message.channel.category_id:
+            if message.channel.category_id == 776641214681841686:
+                await message.channel.edit(position=0)
+
+
+def setup(bot):
+    bot.add_cog(NIT(bot))
